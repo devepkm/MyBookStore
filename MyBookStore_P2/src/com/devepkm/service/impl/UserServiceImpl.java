4ccb28072b1415fede37a23798384d171afa7cda
devepkm <devepkm@gmail.com>
2020-09-20 21:45:26 +0800

    impl and test UserService.java, UserServiceImpl.java

---
+++ MyBookStore_P2/src/com/devepkm/service/impl/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package com.devepkm.service.impl;
+
+import com.devepkm.bean.User;
+import com.devepkm.dao.UserDao;
+import com.devepkm.dao.impl.UserDaoImpl;
+import com.devepkm.service.UserService;
+
+/**
+ * @Auther: dev
+ * @Date: 20/9/2020 21:36
+ * @Description:
+ */
+public class UserServiceImpl implements UserService {
+    UserDao userDao = new UserDaoImpl();
+
+    @Override
+    public boolean signUp(User u) {
+        return userDao.saveUser(u);
+    }
+
+    @Override
+    public boolean login(User u) {
+        User t = userDao.verifyUser(u.getUsername(), u.getPassword());
+        return t == null ? false : true;
+    }
+
+    @Override
+    public boolean isUsernameExits(String username) {
+        User t = userDao.queryUserByUsername(username);
+        return t == null ? false : true;
+    }
+}
