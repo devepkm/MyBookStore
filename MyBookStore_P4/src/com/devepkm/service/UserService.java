feccf01e3c1a5e10695be9bc3a0518e79bfd24d7
devepkm <devepkm@gmail.com>
2020-10-01 12:14:40 +0800

    imp and test BookDao, Book

---
+++ MyBookStore_P4/src/com/devepkm/service/UserService.java
@@ -0,0 +1,18 @@
+package com.devepkm.service;
+
+import com.devepkm.bean.User;
+
+/**
+ * @Auther: dev
+ * @Date: 20/9/2020 21:32
+ * @Description: UserService for sign up, login, checking username availability
+ */
+public interface UserService {
+
+    public boolean signUp(User u);
+
+    public boolean login(User u);
+
+    public boolean isUsernameExits(String username);
+
+}
